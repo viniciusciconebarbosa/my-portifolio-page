08817cf1c0338f3627daf5db4891a676bf7f541a
viniciusciconebarbosa <92874088+viniciusciconebarbosa@users.noreply.github.com>
2026-03-06 21:48:35 -0300

    Update sitemap URL from .tech to .app

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.viniciusbarbosadev.tech', // Sua URL principal
+      url: 'https://www.viniciusbarbosadev.app', // Sua URL principal
       lastModified: new Date(), // Data de hoje
       changeFrequency: 'monthly', // Com que frequência a página muda?
       priority: 1, // A prioridade máxima (1.0) para sua página inicial
